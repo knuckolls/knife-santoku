7b88627d5dbac05a023d790696794b0eedd8ecba
Kevin Nuckolls <kevin.nuckolls@gmail.com>
2012-05-15 11:48:14 -0500

    should only be active when santoku is configured

--- lib/knife_santoku/application.rb
+++ lib/knife_santoku/application.rb
@@ -4,7 +4,8 @@ require 'chef/knife'
 module KnifeSantoku
   class Application
     def initialize(argv)
-      
+      @active = false
+            
       chef_conf_dir = ::Chef::Knife.chef_config_dir
       
       config_file = "#{chef_conf_dir}/../config/santoku-config.yml"
@@ -12,27 +13,28 @@ module KnifeSantoku
       after_file  = "#{chef_conf_dir}/../config/santoku-after.rb"
       lib_folder  = Pathname.new("#{chef_conf_dir}/../lib/santoku/")
       
+      if File.exists?(config_file) and (File.exists?(before_file) or File.exists?(after_file))
+        @active = true
+        
+        config = AppConf.new
+        config.load(config_file)
 
-      config = AppConf.new
-      config.load(config_file)
-
-      
-      # load up notifiers & custom code out of lib
-      #notifier = Notifier.new(config,lib_folder)
-      notifier = ::KnifeSantoku::Notifier.new(config)
-      
-      
-      # load up callbacks
-      @before_callbacks = Callback.new(before_file, notifier, argv)
-      @after_callbacks  = Callback.new(after_file, notifier, argv)
+        # load up notifiers & custom code out of lib
+        #notifier = Notifier.new(config,lib_folder)
+        notifier = ::KnifeSantoku::Notifier.new(config)
+            
+        # load up callbacks
+        @before_callbacks = Callback.new(before_file, notifier, argv)
+        @after_callbacks  = Callback.new(after_file, notifier, argv)
+      end
     end
     
     def run_before_callbacks
-      @before_callbacks.run
+      @before_callbacks.run if @active
     end
     
     def run_after_callbacks
-      @after_callbacks.run
+      @after_callbacks.run if @active
     end
   end
 end
